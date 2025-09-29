bf21c3fd276acd12643ae7b7233b5c7cf419e604
bigczapa <czokapikdx2@gmail.com>
2025-09-29 13:09:37 +0200

    wypisywanie tablicy w metodzie

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,23 @@ public class Main {
 
 
         //wypisz elementy tablicy
+       wypisz(tablicaWylosowanychLiczb);
+        System.out.println();
 
-        System.out.println("Wylosowana tablica:");
-        for (int i = 0; i < tablicaWylosowanychLiczb.length; i++) {
-            System.out.print(tablicaWylosowanychLiczb[i]+" , ");
+        //losuj 30 liczb z zakresu 1,40 bez powtorzen
 
-        }
+
+
     }
+
+    private static void  wypisz(int[] tablica){
+        System.out.println("Wylosowana tablica:");
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println(tablica[i]+", ");
+        }
+
+    }
+
     private static int[] losujLiczbyDoTablicy(int ileLiczb){
         int[] liczbyWylosowane = new int[ileLiczb];
         Random random = new Random();
