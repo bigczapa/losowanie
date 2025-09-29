b79188806689a6c874d4b10c80accf6fb03e85fb
bigczapa <czokapikdx2@gmail.com>
2025-09-29 13:27:23 +0200

    wszystko z lekcji 29.09

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,61 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
         //losuj 10 liczb do tablicy
-
-        int[] tablicaWylosowanychLiczb = losujLiczbyDoTablicy(5);
-        Random random = new Random();
-        for (int i = 0; i < tablicaWylosowanychLiczb.length; i++) {
-            tablicaWylosowanychLiczb[i] = random.nextInt(100)+1;
-        }
-
+        int[] TablicaWylosowanychLiczb = LosujLiczbyDoTablicy(5);
 
         //wypisz elementy tablicy
-       wypisz(tablicaWylosowanychLiczb);
+        wypisz(TablicaWylosowanychLiczb);
         System.out.println();
 
         //losuj 30 liczb z zakresu 1,40 bez powtorzen
 
-
+        ArrayList<Integer> wylosowaneLiczby = wylosujLiczbyBezPowtorzenDoListy(30);
+        wypisz(wylosowaneLiczby);
 
     }
+    private static ArrayList<Integer> wylosujLiczbyBezPowtorzenDoListy(int ileLiczb){
+        ArrayList<Integer> liczbyWylosowane = new ArrayList<>();
+        //nie musi być znany rozmiar listy
+        // może być zmieniony w trakcie działania programua
+        //w każdej kolekcji tylko typy złożone
+        //typ zlozony pisany wielką literą
 
-    private static void  wypisz(int[] tablica){
+        Random random = new Random();
+        for (int i = 0; i < ileLiczb; i++) {
+            int liczba = random.nextInt(40)+1;
+            while(liczbyWylosowane.contains(liczba)){
+                liczba = random.nextInt(40)+1;
+            }
+            liczbyWylosowane.add(random.nextInt(40)+1);
+        }
+        return liczbyWylosowane;
+    };
+    private static void wypisz(int[] tablica){
         System.out.println("Wylosowana tablica:");
         for (int i = 0; i < tablica.length; i++) {
-            System.out.println(tablica[i]+", ");
-        }
+            System.out.println(tablica[i] + " ");
 
+        }
     }
 
-    private static int[] losujLiczbyDoTablicy(int ileLiczb){
+    private static void wypisz(ArrayList<Integer> lista){
+        System.out.println("Wypisana lista:");
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i)+", ");
+        }
+    }
+
+    private static int[] LosujLiczbyDoTablicy(int ileLiczb){
         int[] liczbyWylosowane = new int[ileLiczb];
         Random random = new Random();
         for (int i = 0; i < liczbyWylosowane.length; i++) {
             liczbyWylosowane[i] = random.nextInt(100)+1;
         }
+
         return liczbyWylosowane;
     }
 }
